ee89301c543b3204d9e829872a34b8735c782e9d
Mayank Joshi <bbmmack.30@gmail.com>
2025-09-13 09:10:30 +0530

    Added coin-flip-game.bat to simulate 5 tosses and declare winner

---
+++ coin-flip-game.bat
@@ -0,0 +1,29 @@
+@echo off
+echo =================
+echo day 4 - flip coin
+echo =================
+setlocal enabledelayedexpansion
+
+
+set /a head=0
+set /a tail=0
+
+for /l %%i in (1,1,5) do (
+    set /a coin=!random!%%2
+
+    if !coin! EQU 0 (
+        set /a head+=1
+    ) else (
+       set /a tail+=1
+    )
+)
+echo Total Heads: !head!
+echo Total Tails: !tail!
+echo  -------------------                    
+
+if !head! GTR !tail! (
+    echo     Head WINS!!!
+) else (
+    echo     Tail WINS!!!
+)
+pause
